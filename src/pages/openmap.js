f981ff85aec3dfb3846855ecba1e6988109dc535
Wreck-X <hemanthkrishnakumar@gmail.com>
2023-04-05 18:59:58 +0530

    bruh

--- src/pages/openmap.js
+++ src/pages/openmap.js
@@ -1,14 +1,14 @@
 import { Icon } from 'leaflet';
 import 'leaflet/dist/leaflet.css'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import { iconPerson } from '../assets/icon';
+
 function Map() {
   return (
     <MapContainer center={[51.505, -0.09]} zoom={13} minZoom={3} scrollWheelZoom={true} style={{ height: '150vh'}}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       <Marker position={[51.505, -0.09]}>
         <Popup>
-          <iconPerson/>
+          {/* <iconPerson/> */}
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
       </Marker>
