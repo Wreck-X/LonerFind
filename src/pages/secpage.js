4c63c361400df3b2a77e597af23d957cdb6a8032
Wreck-X <hemanthkrishnakumar@gmail.com>
2023-04-05 13:04:16 +0530

    bleh

---
+++ src/pages/secpage.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Secpage() {
+  return (
+    <div class="animation_layer parallax" id="secpage">
+      <div className='Test'>
+        <a className='Heading'>Why us?</a>
+        <div className='para1'>
+          <p id='paratext'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <a className='tryhead'>Try now!</a>
+        <div id='butdiv'>
+          <button className='button1'>Something</button>
+        </div>
+        <div className='carddiv'>
+          <div id='card1'></div>
+          <div id='card2'></div>
+        </div>
+      </div>
+    </div>
+  )
+}
