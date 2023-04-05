a3e7d000f5c807f2e0d0547edf1f261eb7569144
BiscuitBobby <87699062+BiscuitBobby@users.noreply.github.com>
2023-04-05 18:54:05 +0530

    retrieve current location

--- src/pages/openmap.js
+++ src/pages/openmap.js
@@ -1,12 +1,22 @@
 import { Icon } from 'leaflet';
 import 'leaflet/dist/leaflet.css'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-
+import React, {useEffect, useState} from "react";
 function Map() {
+    const [location, setLocation] = useState({ lat: 0, lng: 0 });
+
+    useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLocation({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      });
+    });
+  }, []);
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} minZoom={3} scrollWheelZoom={true} style={{ height: '150vh'}}>
+    <MapContainer center={[location.lat, location.lng]} zoom={13} maxZoom={20} scrollWheelZoom={true} style={{ height: '150vh'}}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={[location.lat, location.lng]}>
         <Popup>
           {/* <iconPerson/> */}
           A pretty CSS3 popup. <br /> Easily customizable.
